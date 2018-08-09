ebe1a3efa59c91c4a1a417c06295cb4553eb0d17
Andriy Dzikh <andriydzikh@gmail.com>
2018-08-09 11:00:27 -0400

    Improved comments significantly. Disabled pinch to prevent the wrong thing from being scaled. Fixed a bug where the lines were not being culled correctly.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Constants
-const distanceForLine = 100;
+const distanceForLine = 10;
 
 // State
 
@@ -55,13 +55,19 @@ function onMouseDown(ev)
 {
 	if(ev.button == 0) // LMB: Draw
 	{
+		// Get the clicked point in SVG coordinates.
 		var pt = doc_display.point(ev.clientX, ev.clientY);
+		// Keep track of the point clicked. This is to detect how far the user has moved.
 		lastDrawPoint = {x: ev.clientX, y: ev.clientY};
+		// Start the plot list.
 		currPlot = "M" + pt.x + " " + pt.y;
+		// Create the path.
+		// TODO: Make attr depend on the current "brush" (as in stroke-width and colour)
 		currDrawPath = doc_display.path(currPlot).attr({fill: "none", stroke: '#000000', "stroke-width": 5});
 	}
 	else if(ev.button == 2) // RMB: Pan
 	{
+		// Just store the start point of the pan so we can later calculate the delta of the pan.
 		panStart = doc_display.point(ev.clientX, ev.clientY);
 	}
 }
@@ -104,23 +110,31 @@ function onMouseUp(ev)
 {
 	if(ev.button == 0) // LMB: Draw
 	{
+		// Calculate the clicked point in SVG coordinates.
 		var pt = doc_display.point(ev.clientX, ev.clientY);
+		// Add the last point to the plot.
 		currPlot += "L" + pt.x + " " + pt.y;
+		// Update the path.
 		currDrawPath.plot(currPlot);
+		// Clear everything.
 		lastDrawPoint = undefined;
 		currDrawPath = undefined;
 		currPlot = undefined;
 	}
 	else if(ev.button == 2) // RMB: Pan
 	{
+		// Get the clicked point in SVG coordinates.
 		var panEnd = doc_display.point(ev.clientX, ev.clientY);
+		// Get how much the pan moved.
 		var deltaPan = {x: panEnd.x - panStart.x, y: panEnd.y - panStart.y};
 
+		// Use the delta to adjust the viewbox.
 		var v = doc_display.viewbox();
 		v.x -= deltaPan.x;
 		v.y -= deltaPan.y;
 		doc_display.viewbox(v);
 
+		// Clear the panning variable.
 		panStart = undefined;
 	}
 }
@@ -155,16 +169,24 @@ function onMouseMove(ev)
 {
 	if(lastDrawPoint) // LMB: Draw
 	{
-		var pt = doc_display.point(ev.clientX, ev.clientY);
-		var delta = {x: pt.x - lastDrawPoint.x, y: pt.y - lastDrawPoint.y};
+		// Get the amount the mouse has moved since the last "waypoint".
+		var delta = {x: ev.clientX - lastDrawPoint.x, y: ev.clientY - lastDrawPoint.y};
 
-		// TODO: May want to replace this with DOM coordinates instead.
+		// TODO: Use Bezier curves instead of lines to draw smooth lines.
+
 		// Is the distance from the last point more than the distance to draw a line?
+		// This is to decrease file size. No need to add redundant points if they are too small to notice.
 		if(delta.x * delta.x + delta.y * delta.y >= distanceForLine * distanceForLine)
 		{
 			// If so, add the new line and set the last draw point to this point.
+
+			// Compute the clicked point in SVG coordinates.
+			var pt = doc_display.point(ev.clientX, ev.clientY);
+			// Add the point to the plot.
 			currPlot += "L" + pt.x + " " + pt.y;
+			// Update the path.
 			currDrawPath.plot(currPlot);
+			// Set the last draw point.
 			lastDrawPoint = {x: ev.clientX, y: ev.clientY};
 		}
 	}
@@ -179,5 +201,10 @@ function onMouseMove(ev)
 		doc_display.viewbox(v);
 	}
 }
-function onPenMove(ev){}
+
+function onPenMove(ev)
+{
+		
+}
+
 function onTouchMove(ev){}
--- main.js
+++ main.js
@@ -3,36 +3,38 @@ const electron = require('electron');
 
 const {app, BrowserWindow} = require('electron')
 
-function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+app.commandLine.appendSwitch('disable-pinch');
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
+function createWindow () {
+	// Create the browser window.
+	win = new BrowserWindow({width: 800, height: 600})
+
+	// and load the index.html of the app.
+	win.loadFile('index.html')
 
 	win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
-    })
-  }
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null
+	})
+}
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow()
-    }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
+		createWindow()
+	}
 })
 
