b5c3d6444da9df1012e702278e280348fa84d477
Andriy <andriydzikh@gmail.com>
2018-08-10 22:47:17 -0400

    Decided to move the sections list out of the config.cfg file and into its own file '.sections'. This way the renderer process is more in control of the sections and pages. Section buttons also look quite nice. Now to let the user add their own sections instead of what I'm currently doing (hard coding the .sections file).

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ const fs = require('fs');
 
 var rootList;
 
+var selectedSection = -1;
+
+function selectSection()
+{
+	var i = parseInt(this.id.substring(7));
+	if(i != selectedSection)
+	{
+		if(selectedSection != -1)
+		{
+			$('#section' + selectedSection).removeClass('selected_section');
+		}
+		selectedSection = i;
+		$('#section' + selectedSection).addClass('selected_section');
+	}
+}
+
+function regenSections()
+{
+	// Start by clearing out the list first.
+	var list = $('#sections_list');
+	list.empty();
+	
+	for(var i = 0; i < rootList.length; i++)
+	{
+		// Create a section button for each element in the rootList.
+		var section = $('<div id="section' + i + '"class="divBtn section_elem">' + rootList[i].name + '</div>');
+		list.append(section);
+		// Add the event listener for the click.
+		section.on("click", selectSection);
+		if(rootList[i].name == selectedSection)
+		{
+			section.addClass('selected_section');
+		}
+	}
+}
+
 function init()
 {
 	// Create the svg view.
@@ -30,12 +66,26 @@ function init()
 	// Assign an arbitrary viewbox.
 	doc_display.viewbox(0,0,1000,1000);
 
+	// Assign an empty root list in case the .sections file was unreadable.
 	rootList = [];
-	fs.readFile("config.cfg", function(err, data){
-		if (err) throw err;
-		rootList = JSON.parse(data).rootFolders;
+	// Try to read the .sections file.
+	fs.readFile(".sections", function(err, data){
+		// If the file has an error, it's possible it just doesn't exist, so we will try to make it exist.
+		if(err)
+		{
+			// Try to write the sections file.
+			fs.writeFile(".sections", JSON.stringify([]), function(err){
+				// If we couldn't create the default sections file, there's nothing we can do.
+				if (err) throw err;
+			});
+			// Return so that we don't try to execute the rest.
+			return;
+		}
+		// If the data is valid, assign the rootList.
+		rootList = JSON.parse(data);
+		// Create the UI for the sections.
+		regenSections();
 	});
-
 }
 
 document.addEventListener("DOMContentLoaded", init);
