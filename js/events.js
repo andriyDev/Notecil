1da709e097bd4acf7f4015d718aff6914da247ba
Andriy <andriydzikh@gmail.com>
2018-08-17 23:54:05 -0400

    Updating the brush colour now updates the selected elements.

--- js/events.js
+++ js/events.js
@@ -11,12 +11,22 @@ function brush_context()
 {
 	var t = $(this);
 	$('#brush_popup').toggleClass('hidden').css({top: t.offset().top + t.height(), left: "" + t.offset().left});
-	
+}
+
+function brush_col_changed()
+{
+	var t = $(this);
+	var val = t.val();
+	for(var i = 0; i < selectedPaths.length; i++)
+	{
+		selectedPaths[i].attr('stroke', val);
+	}
 }
 
 function events_init()
 {
 	$('#toolbar_brush').on('click', brush_click);
 	$('#toolbar_brush').on('contextmenu', brush_context);
+	$('#brush_colour').on('input', brush_col_changed);
 }
 
