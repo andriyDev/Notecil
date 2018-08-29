17c1f139779b23c5e26ec5d7f08cfd7aed80e3d9
Andriy <andriydzikh@gmail.com>
2018-08-29 12:46:26 -0400

    The start point of a path is always drawn. That way when the line is drawn, it will have an end-cap.

--- js/renderer.js
+++ js/renderer.js
@@ -90,15 +90,95 @@ function draw_chunk(port)
 		switch(page_data[i].type)
 		{
 			case TYPE_PATH:
-				draw_path(port, data);
+				draw_path(page_data[i], port, data);
 				break;
 		}
 	}
 	return data;
 }
 
-function draw_path(port, image_data)
+function set_pixel(image_data, colour, pos)
 {
-	
+	var ind = (pos.x + pos.y * image_data.width) * 4;
+	image_data.data[ind] = Math.round(colour.r * 255);
+	image_data.data[ind + 1] = Math.round(colour.g * 255);
+	image_data.data[ind + 2] = Math.round(colour.b * 255);
+	image_data.data[ind + 3] = Math.round(colour.a * 255);
+}
+
+function blend_pixel(image_data, colour, pos)
+{
+	// If the alpha is 1, we can just set since it will be faster.
+	if(colour.a == 1)
+	{
+		set_pixel(image_data, colour, pos);
+		return;
+	}
+	// Compute the index and the alphas.
+	var ind = (pos.x + pos.y * image_data.width) * 4;
+	var dst_alpha = image_data.data[ind + 3] / 255;
+	var new_alpha = colour.a + dst_alpha * (1 - colour.a);
+	// If the new alpha is 0, just turn the pixel transparent-black
+	if(new_alpha == 0)
+	{
+		image_data.data[ind] = 0;
+		image_data.data[ind + 1] = 0;
+		image_data.data[ind + 2] = 0;
+		image_data.data[ind + 3] = 0;
+		return;
+	}
+	// Perform blend calculations.
+	image_data.data[ind] = ((colour.r * colour.a + image_data.data[ind] / 255 * dst_alpha * (1 - colour.a)) / new_alpha * 255);
+	image_data.data[ind + 1] = Math.round((colour.g * colour.a + image_data.data[ind + 1] / 255 * dst_alpha * (1 - colour.a)) / new_alpha * 255);
+	image_data.data[ind + 2] = Math.round(((colour.b * colour.a + image_data.data[ind + 2] / 255 * dst_alpha * (1 - colour.a)) / new_alpha * 255);
+	image_data.data[ind + 3] = Math.round(new_alpha * 255);
+}
+
+function draw_circle(pt, radius, colour, port, image_data)
+{
+	var coords = {x: Math.round((pt.x - port.x) * image_data.width / port.width),
+				y: Math.round((pt.y - port.y) * image_data.height / port.height)};
+	var r = Math.round(radius * image_data.width / port.width);
+	// Loop through a square around coords with radius r to draw a circle.
+	for(var y = Math.max(coords.y - r, 0); y < Math.min(coords.y + r, image_data.height); y++)
+	{
+		for(var x = Math.max(coords.x - r, 0); x < Math.min(coords.x + r, image_data.width); x++)
+		{
+			var d = Math.sqrt(x * x + y * y);
+			if(d - r <= 0)
+			{
+				// If we are within the circle, blend the colour.
+				blend_pixel(image_data, colour, {x: x, y: y});
+			}
+			else if(d - r < 1)
+			{
+				// If we are just on the edge of the circle, we want to set the pixel to be smoothed based
+				// on the subpixel distance.
+				var adjusted_colour = {r: colour.r, g: colour.g, b: colour.b, a: colour.a * (d-r)};
+				blend_pixel(image_data, adjusted_colour, {x: x, y: y});
+			}
+		}
+	}
+}
+
+function draw_path(path, port, image_data)
+{
+	if(path.data.length == 0)
+	{
+		return;
+	}
+	// TODO: Make the cap not overlap the actual path since for transparent paths that can look ugly.
+	// Draw the start point so that the cap is visible.
+	draw_circle(path.data[0], path.width * path.data[0].p, path.colour, port, image_data);
+	if(path.data.length == 1)
+	{
+		return;
+	}
+	// We now know we have at least two valid points.
+
+	for(var i = 0; i < path.data.length; i++)
+	{
+		
+	}
 }
 
